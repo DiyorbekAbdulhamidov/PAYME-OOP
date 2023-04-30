36c68ac97fd62df5a8f1ef8e6ef1451c78964f21
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-04-30 22:54:15 +0500

    finish codes

--- src/entities/user/user.js
+++ src/entities/user/user.js
@@ -2,18 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 var User = /** @class */ (function () {
-    function User(firstName, lastName, phoneNumber, password, isBlocked) {
+    function User(firstName, lastName, phoneNumber, password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
         this.password = password;
-        this.isBlocked = isBlocked;
     }
-    User.prototype.setId = function (id) {
-        this.id = id;
+    ;
+    User.prototype.setId = function (userID) {
+        this.id = userID;
     };
     User.prototype.getId = function () {
         return this.id;
+    };
+    User.prototype.getNumber = function () {
+        return this.phoneNumber;
     };
     return User;
 }());
